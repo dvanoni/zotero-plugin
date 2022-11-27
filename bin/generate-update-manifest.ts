956da4dde34e28343787fc4490f8cd906ca2b0a6
David Hoff-Vanoni <david@vanoni.dev>
2022-11-26 22:42:18 -0800

    feat: Add scripts for generating new manifest files

---
+++ bin/generate-update-manifest.ts
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-console, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions */
+
+import fse from 'fs-extra'
+import path from 'path'
+
+import root from '../root'
+import version from '../version'
+
+const GEN_DIR = 'gen'
+const UPDATES_JSON_PATH = `${GEN_DIR}/updates.json`
+const UPDATE_RDF_PATH = `${GEN_DIR}/update.rdf`
+
+const [,, updateLink] = process.argv
+
+if (!updateLink) {
+  throw new Error('Update link must be provided as first argument')
+}
+
+const pkg = { ...require(path.join(root, 'package.json')) }
+
+if (!pkg.id) {
+  (pkg.id as string) = `${pkg.name.replace(
+    /^zotero-/,
+    ''
+  )}@${pkg.author.email.replace(/.*@/, '')}`.toLowerCase()
+}
+
+console.log(`Generating ${UPDATES_JSON_PATH} and copying to ${UPDATE_RDF_PATH}`)
+
+const updatesJson = {
+  addons: {
+    [pkg.id]: {
+      updates: [
+        {
+          version,
+          update_link: updateLink,
+          applications: {
+            gecko: {
+              strict_min_version: '60.9',
+              strict_max_version: '60.9',
+            },
+            ...(pkg.xpi.supportsZotero7 ? {
+              zotero: {
+                strict_min_version: '6.999',
+                strict_max_version: '7.0.*',
+              },
+            } : null),
+          },
+        },
+      ],
+    },
+  },
+}
+
+fse.outputJsonSync(
+  path.join(root, UPDATES_JSON_PATH),
+  updatesJson,
+  { spaces: 2 }
+)
+fse.copySync(
+  path.join(root, UPDATES_JSON_PATH),
+  path.join(root, UPDATE_RDF_PATH)
+)
---
+++ generate-install-manifest.ts
@@ -0,0 +1,61 @@
+/* eslint-disable no-console, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/restrict-template-expressions */
+
+import fse from 'fs-extra'
+import path from 'path'
+import pug from 'pug'
+
+import root from './root'
+import version from './version'
+
+const BUILD_DIR = 'build'
+const INSTALL_RDF_PATH = `${BUILD_DIR}/install.rdf`
+const MANIFEST_JSON_PATH = `${BUILD_DIR}/manifest.json`
+
+const pkg = { ...require(path.join(root, 'package.json')) }
+
+if (!pkg.id) {
+  (pkg.id as string) = `${pkg.name.replace(
+    /^zotero-/,
+    ''
+  )}@${pkg.author.email.replace(/.*@/, '')}`.toLowerCase()
+}
+if (pkg.xpi) Object.assign(pkg, pkg.xpi)
+
+pkg.version = version
+
+pkg.updateURL = `${pkg.xpi.releaseURL}update.rdf`
+
+const options_and_vars = { ...pkg, pretty: true }
+
+console.log(`Generating ${INSTALL_RDF_PATH}`)
+
+const template = fse.readFileSync(
+  path.join(__dirname, 'install.rdf.pug'),
+  'utf8'
+)
+const installRdf = pug.render(template, options_and_vars)
+fse.outputFileSync(path.join(root, INSTALL_RDF_PATH), installRdf)
+
+console.log(`Generating ${MANIFEST_JSON_PATH}`)
+
+const manifestJson = {
+  manifest_version: 2,
+  name: pkg.name,
+  description: pkg.description,
+  version: pkg.version,
+  homepage_url: pkg.homepage,
+  applications: {
+    zotero: {
+      id: pkg.id,
+      update_url: `${pkg.xpi.releaseURL}updates.json`,
+      strict_min_version: '6.999',
+      strict_max_version: '7.0.*',
+    },
+  },
+}
+
+fse.outputJsonSync(
+  path.join(root, MANIFEST_JSON_PATH),
+  manifestJson,
+  { spaces: 2 }
+)
