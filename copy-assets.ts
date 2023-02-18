c6f0633a38f1bfeba1afc828661ac467a174d496
David Hoff-Vanoni <david@vanoni.dev>
2023-02-18 00:39:57 -0800

    feat: Do not copy `.DS_Store` files and `__tests__` directories

--- copy-assets.ts
+++ copy-assets.ts
@@ -5,17 +5,22 @@ import root from './root'
 
 const IGNORED_EXTENSIONS = ['.json', '.ts', '.tsx']
 
-function includeFile(src: string) {
-  const include = !IGNORED_EXTENSIONS.includes(path.extname(src).toLowerCase())
-  if (include) console.log(src)
-  return include
-}
+const IGNORED_PATHS = /(\.DS_Store|__tests__)$/
 
 const srcDir = path.join(root, 'src')
 const destDir = path.join(root, 'build')
 
 console.group('Copying assets')
 
-fse.copySync(srcDir, destDir, { filter: includeFile })
+fse.copySync(srcDir, destDir, {
+  filter(src) {
+    const include = (
+      !IGNORED_EXTENSIONS.includes(path.extname(src).toLowerCase()) &&
+      !IGNORED_PATHS.test(src)
+    )
+    if (include) console.log(src)
+    return include
+  }
+})
 
 console.groupEnd()
